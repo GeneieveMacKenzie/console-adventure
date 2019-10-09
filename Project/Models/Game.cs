a714caa243b2ba7d16f3ce742970f166ed6a705d
Geneieve MacKenzie <mackenzie.gen@gmail.com>
2019-10-09 16:55:55 -0600

    init setup

--- Project/Models/Game.cs
+++ Project/Models/Game.cs
@@ -4,13 +4,36 @@ namespace ConsoleAdventure.Project.Models
 {
     public class Game : IGame
     {
+
         public IRoom CurrentRoom { get; set; }
         public IPlayer CurrentPlayer { get; set; }
 
         //NOTE Make yo rooms here...
         public void Setup()
         {
-            throw new System.NotImplementedException();
+
+            Room room1 = new Room("Room1", "You entered the first room");
+            Room room2 = new Room("room2", "You have entered room 2");
+            Room room3 = new Room("room3", "You have entered room 3");
+            Room room4 = new Room("room4", "You have entered room 4");
+            Room room5 = new Room("room5", "You have entered room 5");
+
+            room1.AddExit(room2);
+            room2.AddExit(room1);
+
+            room2.AddExit(room3);
+            room2.AddExit(room4);
+            room4.AddExit(room2);
+            room4.AddExit(room5);
+            
+
+
+            //Add Exits
+        }
+        public Game()
+        {
+            Player = new Player();
+            Setup();
         }
     }
 }
--- Project/Models/Room.cs
+++ Project/Models/Room.cs
@@ -5,9 +5,22 @@ namespace ConsoleAdventure.Project.Models
 {
     public class Room : IRoom
     {
+
         public string Name { get; set; }
         public string Description { get; set; }
         public List<Item> Items { get; set; }
         public Dictionary<string, IRoom> Exits { get; set; }
+
+        public void AddExit(IRoom room)
+        {
+            Exits.Add(room.Name, room);
+        }
+        public Room(string name, string description)
+        {
+            Name = name;
+            Description = description;
+            Items = new List<Item>();
+            Exits = new Dictionary<string, IRoom>();
+        }
     }
 }
